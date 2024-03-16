46858ce74cd6063d4da9ca16b67f5ea4382c4467
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-03-16 22:51:42 +0900

    #1-2 テストを修正

--- KoeBook.Test/DiTestBase.cs
+++ KoeBook.Test/DiTestBase.cs
@@ -17,6 +17,6 @@ public class DiTestBase
     {
         return Microsoft.Extensions.Hosting.Host
             .CreateDefaultBuilder()
-            .ConfigureCore();
+            .UseCoreStartup();
     }
 }
