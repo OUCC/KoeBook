9a57021815ddd0ea211df34694760db88fb06d35
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-04-03 21:27:42 +0900

    #1-3 テストを追加

--- KoeBook.Test/Epub/ScrapingAozoraServiceTest.cs
+++ KoeBook.Test/Epub/ScrapingAozoraServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using AngleSharp;
 using AngleSharp.Dom;
+using KoeBook.Epub.Models;
 using KoeBook.Epub.Services;
 
 namespace KoeBook.Test.Epub;
@@ -11,20 +12,51 @@ public class ScrapingAozoraServiceTest
     [InlineData("", "")]
     public async Task TextProcess(string input, string expected)
     {
-        using var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+        using var context = BrowsingContext.New(Configuration.Default);
         using var doc = await context.OpenAsync(req => req.Content(input));
-
         Assert.NotNull(doc.ParentElement);
-        var result = ScrapingAozora.TextProcess(doc.ParentElement!);
+
+        var result = ScrapingAozora.TextProcess(null, doc.ParentElement!);
 
         Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("", new[] { "" })]
+    public async Task AddParagraphs1(string input, string[] expected)
+    {
+        using var context = BrowsingContext.New(Configuration.Default);
+        using var doc = await context.OpenAsync(req => req.Content(input));
+        Assert.NotNull(doc.ParentElement);
+        var epubDocument = new EpubDocument("title", "author", "", default)
+        {
+            Chapters = [new() { Sections = [new("section title") { Elements = [new Paragraph() { Text = "test" }] }] }]
+        };
+
+        Assert.Equal(expected.Length, epubDocument.Chapters[0].Sections[0].Elements.Count);
+        Assert.All(epubDocument.Chapters[0].Sections[0].Elements.Zip(expected), v =>
+        {
+            var (element, expected) = v;
+            var paragraph = Assert.IsType<Paragraph>(element);
+            Assert.Equal(expected, paragraph.Text);
+        });
     }
 }
 
 file static class ScrapingAozora
 {
     [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
-    private static extern string TextProcess(ScrapingAozoraService? _, IElement element);
+    public static extern string TextProcess(ScrapingAozoraService? _, IElement element);
 
-    public static string TextProcess(IElement element) => TextProcess(null, element);
+    [UnsafeAccessor(UnsafeAccessorKind.Method)]
+    public static extern void AddParagraphs(ScrapingAozoraService service, List<KoeBook.Epub.Models.Element> focusElements, IElement element, bool lastEmpty);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method)]
+    public static extern void AddParagraphs(ScrapingAozoraService service, List<KoeBook.Epub.Models.Element> focusElements, string input, bool lastEmpty);
+
+    [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
+    public static extern string TextReplace(ScrapingAozoraService? _, string text);
+
+    [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
+    public static extern (List<int> contentsIds, bool hasChapter, bool hasSection) LoadToc(ScrapingAozoraService? _, IDocument doc, EpubDocument epubDocument);
 }
