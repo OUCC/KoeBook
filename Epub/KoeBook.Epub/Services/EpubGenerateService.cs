cbbbb663ac794be516b8cd2a0cab786a055eb64c
TakenPt <takekimu14@gmail.com>
2024-05-02 23:02:18 +0900

    #51 音声合成にかける文字数を制限する＆wavからmp3へ変換

--- Epub/KoeBook.Epub/Services/EpubGenerateService.cs
+++ Epub/KoeBook.Epub/Services/EpubGenerateService.cs
@@ -3,6 +3,7 @@ using KoeBook.Core.Contracts.Services;
 using KoeBook.Core.Models;
 using KoeBook.Epub.Contracts.Services;
 using KoeBook.Epub.Models;
+using NAudio.Wave;
 
 namespace KoeBook.Epub.Services;
 
@@ -17,10 +18,17 @@ public class EpubGenerateService(ISoundGenerationService soundGenerationService,
         cancellationToken.ThrowIfCancellationRequested();
 
         var document = _documentStoreService.Documents.Single(d => d.Id == bookScripts.BookProperties.Id);
+        var tmpMp3Path = Path.Combine(tempDirectory, "temp.mp3");
 
         foreach (var scriptLine in bookScripts.ScriptLines)
         {
-            scriptLine.Audio = new Audio(await _soundGenerationService.GenerateLineSoundAsync(scriptLine, bookScripts.Options, cancellationToken).ConfigureAwait(false));
+            var wavData = await _soundGenerationService.GenerateLineSoundAsync(scriptLine, bookScripts.Options, cancellationToken).ConfigureAwait(false);
+            var ms = new MemoryStream();
+            ms.Write(wavData);
+            ms.Position = 0;
+            using var reader = new WaveFileReader(ms);
+            MediaFoundationEncoder.EncodeToMp3(reader, tmpMp3Path);
+            scriptLine.Audio = new Audio(File.ReadAllBytes(tmpMp3Path));
         }
 
         if (await _createService.TryCreateEpubAsync(document, tempDirectory, cancellationToken).ConfigureAwait(false))
