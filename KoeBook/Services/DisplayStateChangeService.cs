53ca42337c23768bf705c3d671052007664def50
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-05-02 14:38:47 +0900

    #41 State更新の条件を追加

--- KoeBook/Services/DisplayStateChangeService.cs
+++ KoeBook/Services/DisplayStateChangeService.cs
@@ -1,6 +1,7 @@
 ﻿using KoeBook.Contracts.Services;
 using KoeBook.Core.Contracts.Services;
 using KoeBook.Core.Models;
+using Microsoft.UI.Dispatching;
 
 namespace KoeBook.Services;
 
@@ -11,7 +12,7 @@ internal class DisplayStateChangeService(IGenerationTaskService taskService) : I
     public void UpdateProgress(BookProperties bookProperties, int progress, int maximum)
     {
         var taskService = _taskService; // thisをキャプチャしないようにする
-        _ = App.MainWindow.DispatcherQueue.TryEnqueue(() =>
+        _ = App.MainWindow.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High, () =>
         {
             var task = taskService.GetProcessingTask(bookProperties.Id);
             task.MaximumProgress = maximum;
@@ -22,9 +23,11 @@ internal class DisplayStateChangeService(IGenerationTaskService taskService) : I
     public void UpdateState(BookProperties bookProperties, GenerationState state)
     {
         var taskService = _taskService; // thisをキャプチャしないようにする
-        _ = App.MainWindow.DispatcherQueue.TryEnqueue(() =>
+        _ = App.MainWindow.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High, () =>
         {
-            taskService.GetProcessingTask(bookProperties.Id).State = state;
+            var task = taskService.GetProcessingTask(bookProperties.Id);
+            if(task.State < state)
+                task.State = state;
         });
     }
 
